3871261f6915b44faf7de5586b064db09401ae39
Vinayak K <vinayakk@adobe.com>
2020-03-02 19:15:39 +0530

    Add k8 deployment and service files

---
+++ k8s/aws/terraform.tfvars
@@ -0,0 +1,4 @@
+cluster_name = "udagram-dev"
+aws_region = "us-east-1"
+control_plane_type = "t3.xlarge"
+worker_type = "t3.xlarge"
---
+++ k8s/deploy.sh
@@ -0,0 +1,14 @@
+# create configmap: env-config
+kubectl apply -f env-config.yaml
+
+# create env-secret
+kubectl apply -f db-secrets.yaml
+
+# create AWS secrets
+kubectl apply -f aws-secrets.yaml
+
+# create deployments
+kubectl apply -f backend-feed-deployment.yaml
+kubectl apply -f backend-user-deployment.yaml
+kubectl apply -f frontend-deployment.yaml
+kubectl apply -f reverseproxy-deployment.yaml
