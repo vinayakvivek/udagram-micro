91d7426ac328c5a0765ae2c6adbf2bc6e1bdfd30
Vinayak <vinayakdeepu@gmail.com>
2020-03-08 11:11:20 +0530

    Add kubectl rollout

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,7 @@ docker push $DOCKER_USERNAME/udacity-user-restapi
 docker push $DOCKER_USERNAME/udacity-feed-restapi
 docker push $DOCKER_USERNAME/udacity-frontend
 docker push $DOCKER_USERNAME/udacity-reverse-proxy
+kubectl rollout restart deployment/backend-user
+kubectl rollout restart deployment/backend-feed
+kubectl rollout restart deployment/frontend
+kubectl rollout restart deployment/reverseproxy
